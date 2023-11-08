7d983ec24c989970af6800cae4a6a7b2d508eb9b
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2023-11-08 10:23:08 -0800

    First changes

---
+++ json.py
@@ -0,0 +1,15 @@
+#Imports the json library
+import json
+
+
+#Create the data dictionary
+
+data = {
+
+    'name': 'Jarod Atienzo',
+    'age':20,
+    'city':'Seattle,WA',
+    'interest':['Gym', 'Running', 'videogames'],
+    'is_student':True
+
+}
