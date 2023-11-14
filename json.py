d933e841d62cfbd5839939a57634f2a5f7272339
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2023-11-14 10:28:33 -0800

    Modified interests data

--- json.py
+++ json.py
@@ -13,7 +13,7 @@ data = {
     'is_student':True
 
 }
-##
+#Opens and writes in a seperate json file
 with open('data.json','w') as json_file:
 
     json.dump(data,json_file,indent=4)
@@ -24,7 +24,18 @@ print('Data has been written to data.json')
 #Reading for the data
 with open('data.json', 'r') as json_file:
     
-    loaded_data= json.load(json_file)
+    loaded_data = json.load(json_file)
 
 print("Successfully able to read data.json")
 print(loaded_data)
+
+#Modifying the json data
+loaded_data['age'] = 21
+loaded_data['interests'].append('Basketball')
+
+#Updates the data.json file
+with open('data.json', 'w') as json_file:
+
+    json.dump(loaded_data, json_file, indent=4)
+
+print('Modified data written to data.json')
